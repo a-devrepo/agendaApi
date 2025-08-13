3f0957e0325a8e98afaeda8a397aaa218a1abf45
alison <adevpr11@gmail.com>
2025-08-13 12:54:47 -0300

    chore: adiciona classe que faz conexão com o banco de dados

---
+++ src/main/java/br/com/nca/factories/ConnectionFactory.java
@@ -0,0 +1,18 @@
+package br.com.nca.factories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	private static final String HOST = "jdbc:postgresql://localhost:5434/bd_apiagenda";
+	private static final String USER = "postgresuser";
+	private static final String PASSWORD = "postgrespassword";
+	private static final String DRIVER = "org.postgresql.Driver";
+	
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+			Class.forName(DRIVER);
+			return DriverManager.getConnection(HOST, USER, PASSWORD);
+	}
+}
