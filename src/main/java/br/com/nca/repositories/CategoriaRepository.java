126e660d3442e0905f156ef2f12326c772252075
alison <adevpr11@gmail.com>
2025-08-15 10:32:51 -0300

    feat: adiciona CategoriaRepository

---
+++ src/main/java/br/com/nca/repositories/CategoriaRepository.java
@@ -0,0 +1,35 @@
+package br.com.nca.repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import br.com.nca.entities.Categoria;
+import br.com.nca.factories.ConnectionFactory;
+
+public class CategoriaRepository {
+
+	public List<Categoria> findAll() throws Exception {
+
+		var sql = """
+				SELECT id, nome from categoria
+				order by nome ASC
+				""";
+		
+		var connection = ConnectionFactory.getConnection();
+		
+		var statement = connection.prepareStatement(sql);
+		
+		var result = statement.executeQuery();
+		
+		var categorias = new ArrayList<Categoria>();
+		
+		while (result.next()) {
+			var id = UUID.fromString(result.getString("id"));
+			var nome = result.getString("nome");
+			var categoria = new Categoria(id, nome);
+			categorias.add(categoria);
+		}
+		return categorias;
+	}
+}
---
+++ src/main/java/br/com/nca/repositories/TarefaRepository.java
@@ -0,0 +1,5 @@
+package br.com.nca.repositories;
+
+public class TarefaRepository {
+
+}
