93b9a4d6823eb6fbf644ebfe0cd1dfecad22738d
alison <adevpr11@gmail.com>
2025-08-20 11:19:25 -0300

    feat(tarefa): adiciona TarefaPostRequest

---
+++ src/main/java/br/com/nca/dtos/TarefaPostRequest.java
@@ -0,0 +1,14 @@
+package br.com.nca.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class TarefaPostRequest {
+
+	private String nome;
+	private String data;
+	private String prioridade;
+	private String categoriaId;
+}
