076a63648492abda32d627580a0a340dd1bb113c
alison <adevpr11@gmail.com>
2025-08-27 09:55:28 -0300

    feat: adiciona DTO TarefaPutRequest

---
+++ src/main/java/br/com/nca/dtos/TarefaPutRequest.java
@@ -0,0 +1,15 @@
+package br.com.nca.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TarefaPutRequest {
+
+	private String nome;
+	private String data;
+	private String prioridade;
+	private String categoriaId;
+	private Boolean finalizada;
+}
