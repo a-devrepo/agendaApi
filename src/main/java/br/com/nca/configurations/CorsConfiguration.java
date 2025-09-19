5419e16fccf1b3d8a570e78aba179c89334a133a
alison <adevpr11@gmail.com>
2025-09-19 15:16:42 -0300

    chore: adiciona configuração de CORS para documentação swagger

--- src/main/java/br/com/nca/configurations/CorsConfiguration.java
+++ src/main/java/br/com/nca/configurations/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOrigins("http://localhost:4200")
+        .allowedOrigins("http://localhost:4200", "http://localhost:8084")
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowedHeaders("*")
         .allowCredentials(true);
